29c966d3c4fae726d354c10cdae941d04da5aeeb
rainiwu <rainiwu01@gmail.com>
2023-11-08 01:55:12 -0800

    rewrite streaming_channelizer

--- streaming_channelizer/src/lib.rs
+++ streaming_channelizer/src/lib.rs
@@ -1,179 +1,129 @@
 use bessel_fun_sys::bessel_func;
-use libm::sinf;
-use libm::sqrtf;
-use num::complex::Complex64;
+
 use num::Complex;
-use rustfft::algorithm::Radix4;
-use rustfft::Fft;
-use std::collections::VecDeque;
-use std::f32::consts::PI;
+use rustfft::{Fft, FftPlanner};
 
-pub struct Queue<T> {
-    buffer: VecDeque<T>,
-    capacity: usize,
-}
+use std::iter::Chain;
+use std::slice::Iter;
+use std::sync::Arc;
 
-impl<T> Queue<T> {
-    pub fn new(capacity: usize) -> Self {
-        Queue {
-            buffer: VecDeque::new(),
-            capacity: capacity,
-        }
-    }
-
-    pub fn add(&mut self, element: T) {
-        if self.buffer.len() < self.capacity {
-            self.buffer.push_back(element);
-        } else {
-            self.buffer.pop_front();
-            self.buffer.push_back(element);
-        }
-    }
-}
-
-/*
- Streaming version of the polyphase channelizer.
- Updates outputs one slice across channels at a time.
- This is maximally decimating.
-*/
-pub struct StreamChannelizer {
-    // This is the number of channels.
-    nchannels: usize,
-
-    // This is the number of taps per channel.
-    ntaps: usize,
-
-    // Filter coefficients in polyphase form
-    coeff: Vec<Vec<Complex<f32>>>,
-
-    // Plan for IFFT
-    plan: Radix4<f32>,
-
-    // Collection of FIFO queues for each channel. Each buffer has capacity = taps.
-    internal_buffers: Vec<Queue<Complex<f32>>>,
-
-    // pre_out_buffer
-    pre_out_buffer: Vec<Complex<f32>>,
-
-    // // Output Buffer : This represents the current one slice output across channels
-    // out_buffer: Vec<Complex<f32>>,
-
-    // Scratch buffer for FFT
-    scratch_buffer: Vec<Complex<f32>>,
-}
-
-pub fn create_filter(taps: usize, channels: usize) -> Vec<Vec<Complex<f32>>> {
-    let mut inter_buffer: Vec<Vec<Complex<f32>>> = Vec::with_capacity(channels);
-    let channel_half = channels / 2;
-    for chann_id in 0..channels {
-        let mut buffer: Vec<Complex<f32>> = vec![Complex::new(0.0, 0.0); 2 * taps];
-        for tap_id in 0..taps {
-            let ind = tap_id * channels + chann_id;
-            if chann_id < channel_half {
-                buffer[2 * tap_id] = Complex::new(channel_fn(ind, channels, taps, 10.0), 0.0);
-            } else {
-                buffer[2 * tap_id + 1] = Complex::new(channel_fn(ind, channels, taps, 10.0), 0.0);
-            }
-        }
-        inter_buffer.push(buffer);
-    }
-    inter_buffer
-}
-
-pub fn create_state(taps: usize, channels: usize) -> Vec<Queue<Complex<f32>>> {
-    let mut outp: Vec<Queue<Complex<f32>>> = Vec::new();
-    for chann in 0..(channels / 2) {
-        outp.push(Queue::new(2 * taps));
-    }
-    outp
-}
-
-pub fn channel_fn(ind: usize, nchannel: usize, nproto: usize, kbeta: f32) -> f32 {
+fn channel_fn(ind: usize, nchannel: usize, nproto: usize, kbeta: f32) -> f32 {
     let ind_arg = ind as f32;
     let arg = -((nproto / 2) as f32) + (ind_arg + 1.0) / (nchannel as f32);
     let darg = (2.0 * ind_arg) / ((nchannel * nproto) as f32) - 1.0;
-    let carg = kbeta * sqrtf(1.0 - darg * darg);
+    let carg = kbeta * (1.0 - darg * darg).sqrt();
     (unsafe { bessel_func(carg) }) / (unsafe { bessel_func(kbeta) })
-        * (if arg != 0.0 { sinf(arg) / arg } else { 1.0 })
+        * (if arg != 0.0 { arg.sin() / arg } else { 1.0 })
 }
 
-impl StreamChannelizer {
-    pub fn new(taps: usize, channels: usize) -> Self {
-        StreamChannelizer {
-            nchannels: channels,
-            ntaps: taps,
-            coeff: create_filter(taps, channels),
-            plan: Radix4::new(channels, rustfft::FftDirection::Inverse),
-            internal_buffers: create_state(taps, channels),
-            pre_out_buffer: vec![Complex::new(0.0, 0.0); channels],
-            scratch_buffer: vec![Complex::new(0.0, 0.0); channels],
+fn create_filter<const TWICE_TAPS: usize, const CHANNELS: usize>() -> Vec<[Complex<f32>; TWICE_TAPS]>
+{
+    let mut result = vec![[Complex::new(0.0, 0.0); TWICE_TAPS]; CHANNELS];
+    let taps = TWICE_TAPS / 2;
+    for chann_id in 0..CHANNELS {
+        let buffer = &mut result[chann_id];
+        for tap_id in 0..taps {
+            let ind = tap_id * CHANNELS + chann_id;
+            if chann_id < CHANNELS / 2 {
+                buffer[2 * tap_id] = Complex::new(channel_fn(ind, CHANNELS, taps, 10.0), 0.0);
+            } else {
+                buffer[2 * tap_id + 1] = Complex::new(channel_fn(ind, CHANNELS, taps, 10.0), 0.0);
+            }
         }
     }
-    pub fn process(&mut self, sample_arr: &[Complex<f32>], output_buffer: &mut [Complex<f32>]) {
-        self.internal_buffers
-            .iter_mut()
-            .enumerate()
-            .for_each(|(ind, item)| item.add(sample_arr[self.nchannels / 2 - ind - 1]));
+    result
+}
 
-        (self.pre_out_buffer)
-            .iter_mut()
-            .enumerate()
-            .for_each(|(ind, item)| {
-                (*item) = buffer_process(&self.internal_buffers, &self.coeff, ind)
-            });
-        self.plan.process_outofplace_with_scratch(
-            &mut self.pre_out_buffer,
-            output_buffer,
-            &mut self.scratch_buffer,
-        )
+#[derive(Copy, Clone, Debug)]
+struct Ring<T, const CAPACITY: usize> {
+    head: usize,
+    full: bool,
+    buffer: [T; CAPACITY],
+}
+
+impl<T: Default + Copy, const CAPACITY: usize> Ring<T, CAPACITY> {
+    fn new() -> Self {
+        Self {
+            head: 0,
+            full: false,
+            buffer: [T::default(); CAPACITY],
+        }
+    }
+
+    fn add(&mut self, element: T) {
+        self.buffer[self.head] = element;
+        self.head += 1;
+        if self.head >= CAPACITY {
+            self.head = 0;
+            self.full = true;
+        }
+    }
+
+    fn inner_iter(&self) -> Chain<Iter<'_, T>, Iter<'_, T>> {
+        let initial = self.buffer[..self.head].iter();
+        if self.full {
+            return initial.chain(self.buffer[self.head..].iter());
+        }
+        initial.chain(self.buffer[0..0].iter())
     }
 }
 
-pub fn buffer_process(
-    lhs: &Vec<Queue<Complex<f32>>>,
-    rhs: &Vec<Vec<Complex<f32>>>,
-    id: usize,
-) -> Complex<f32> {
-    let mut sum = Complex { re: 0.0, im: 0.0 };
-    let nchannels = rhs.len();
-    // println!("{}", nchannels);
-    let reduced_ind = if id < nchannels / 2 {
-        id
-    } else {
-        id - nchannels / 2
-    };
-    for (ind, item) in lhs[reduced_ind].buffer.iter().enumerate() {
-        sum += (*item) * rhs[id][ind];
-    }
-    sum
+pub struct Channelizer<const TWICE_TAPS: usize, const CHANNELS: usize> {
+    fft: Arc<dyn Fft<f32>>,
+    coeff: Vec<[Complex<f32>; TWICE_TAPS]>,
+    state: Vec<Ring<Complex<f32>, TWICE_TAPS>>,
+    buff: [Complex<f32>; CHANNELS],
+    scratch: [Complex<f32>; CHANNELS],
 }
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+impl<const TWICE_TAPS: usize, const CHANNELS: usize> Channelizer<TWICE_TAPS, CHANNELS> {
+    pub fn new() -> Self {
+        Self {
+            fft: FftPlanner::new().plan_fft_inverse(CHANNELS),
+            coeff: create_filter::<TWICE_TAPS, CHANNELS>(),
+            state: vec![Ring::new(); CHANNELS / 2],
+            buff: [Complex::new(0.0, 0.0); CHANNELS],
+            scratch: [Complex::new(0.0, 0.0); CHANNELS],
+        }
+    }
+
+    pub fn process(&mut self, samples: &[Complex<f32>], output: &mut [Complex<f32>]) {
+        self.state
+            .iter_mut()
+            .enumerate()
+            .for_each(|(idx, item)| item.add(samples[CHANNELS / 2 - idx - 1]));
+
+        self.buff.iter_mut().enumerate().for_each(|(idx, item)| {
+            *item = Complex::new(0.0, 0.0);
+            let smol_idx = idx % CHANNELS / 2;
+            self.state[smol_idx]
+                .inner_iter()
+                .enumerate()
+                .for_each(|(inner_idx, inner_item)| {
+                    *item += (*inner_item) * self.coeff[idx][inner_idx]
+                });
+        });
+
+        self.fft
+            .process_outofplace_with_scratch(&mut self.buff, output, &mut self.scratch);
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
-    fn it_works() {
-        let channels: usize = 1024;
-        let taps: usize = 128;
 
-        let mut channelizer = StreamChannelizer::new(128, 1024);
+    #[test]
+    fn process() {
+        let mut channelizer = Channelizer::<256, 1024>::new();
 
         // Example input signal: Let's just use a bunch of 1's for simplicity
-        let input_signal = vec![Complex::new(1.0 as f32, 0.0); channels / 2];
+        let input_signal = vec![Complex::new(1.0 as f32, 0.0); 1024 / 2];
 
         // Buffer for the channelizer output
-        let mut output_buffer = vec![Complex::new(0.0 as f32, 0.0); channels];
+        let mut output_buffer = vec![Complex::new(0.0 as f32, 0.0); 1024];
 
         // Process the input signal
         channelizer.process(&input_signal, &mut output_buffer);
-
-        // Print the output buffer
-        for (i, sample) in output_buffer.iter().enumerate() {
-            println!("Channel {}: {:?}", i, sample);
-        }
     }
 }
